d06b56f5483951ab0125374eb3ec4686ae64b3e9
guygubaby <guygubaby@gmail.com>
2021-07-29 10:12:08 +0800

    feat: add cache proxy sample code

--- src/section6/index.ts
+++ src/section6/index.ts
@@ -1,16 +1,33 @@
 const multiply = (...args: number[]): number => {
   return args.reduce((accumulator, current) => {
+    return accumulator * current
+  }, 1)
+}
+
+const sum = (...args: number[]): number => {
+  return args.reduce((accumulator, current) => {
     return accumulator + current
   }, 0)
 }
 
-const cacheMultiply = (() => {
-  const cache: Record<string, number> = Object.create(null)
-  return (...args: number[]) => {
+const createProxyFactory = <T extends string|number>(fn: Function) => {
+  const cache: Record<string, T> = Object.create(null)
+  return (...args: T[]) => {
     const key = args.join('-')
     if (cache[key]) return cache[key]
-    return cache[key] = multiply(...args)
+    return cache[key] = fn(...args)
   }
-})()
+}
 
-cacheMultiply(1, 2, 4)
+const cachedMultiply = createProxyFactory(multiply)
+const cachedSum = createProxyFactory(sum)
+
+const res = cachedMultiply(1, 2, 3, 4)
+console.log('res: ', res)
+const res1 = cachedMultiply(1, 2, 3, 4)
+console.log('res1: ', res1)
+
+const res3 = cachedSum(1, 2, 3, 4)
+console.log('res3: ', res3)
+const res4 = cachedSum(1, 2, 3, 4)
+console.log('res4: ', res4)
